5d6be18e2617a9d4b2bed9365dec7b3098a9959d
mjrdd <mddevilleres@gmail.com>
2022-11-01 17:13:08 +0800

    Set up adapter-node for custom build

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapterAuto from "@sveltejs/adapter-auto";
+import adapterNode from "@sveltejs/adapter-node";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +11,7 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter()
+		adapter: !process.env.BUILD_NODE ? adapterAuto() : adapterNode()
 	}
 };
 
