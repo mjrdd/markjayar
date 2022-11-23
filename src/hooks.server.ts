4cc0e474287cb9ad720d5605ddeaf02add652308
Mark Devilleres <49123306+mjrdd@users.noreply.github.com>
2022-11-23 15:46:20 +0000

    Add devcontainer.json and cleanup hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,28 +1,22 @@
-import { sequence } from "@sveltejs/kit/hooks";
-import type { Handle } from "@sveltejs/kit";
+import { redirect, type Handle } from "@sveltejs/kit";
 
-const socials: Handle = ({ event, resolve }) => {
-	const socials: { [key: string]: string } = {
-		youtube: "https://www.youtube.com/channel/UCn_0Rcmn415xw9cDp_py6dA",
-		twitter: "https://twitter.com/mjayar95",
-		instagram: "https://www.instagram.com/mjayardev",
-		twitch: "https://www.twitch.tv/markjayar",
-		github: "https://github.com/mjrdd",
-		tiktok: "https://www.tiktok.com/@mjayar95"
-	};
+const redirects: {
+	[key: string]: string;
+} = {
+	"/youtube": "https://www.youtube.com/channel/UCn_0Rcmn415xw9cDp_py6dA",
+	"/twitter": "https://twitter.com/mjayar95",
+	"/instagram": "https://www.instagram.com/mjayardev",
+	"/twitch": "https://www.twitch.tv/markjayar",
+	"/github": "https://github.com/mjrdd",
+	"/tiktok": "https://www.tiktok.com/@mjayar95"
+};
 
-	const path = event.url.pathname.substring(1);
+export const handle: Handle = async ({ event, resolve }) => {
+	const path = event.url.pathname;
 
-	if (socials[path]) {
-		return new Response(null, {
-			status: 303,
-			headers: {
-				location: socials[path]
-			}
-		});
+	if (path in redirects) {
+		throw redirect(303, redirects[path]);
 	}
 
 	return resolve(event);
 };
-
-export const handle = sequence(socials);
