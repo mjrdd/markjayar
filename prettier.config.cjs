0a13040478b9eb8ad917dbef92303e4b451d0fd7
mjrdd <mddevilleres@gmail.com>
2022-12-17 22:09:55 +0800

    Update prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,13 +3,20 @@ const config = {
 	useTabs: true,
 	trailingComma: "none",
 	printWidth: 100,
-	bracketSameLine: true,
 	plugins: [
 		// require("prettier-plugin-svelte")
 		require("prettier-plugin-tailwindcss")
 	],
 	pluginSearchDirs: ["."],
-	overrides: [{ files: "*.svelte", options: { parser: "svelte" } }]
+	overrides: [
+		{
+			files: ["*.svelte"],
+			options: {
+				parser: "svelte",
+				bracketSameLine: true
+			}
+		}
+	]
 };
 
 module.exports = config;
