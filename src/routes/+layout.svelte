33e75ab856cdbdcd40bfc3e0320b36c1dc9fced8
Mark Devilleres <enigmaticjayar@gmail.com>
2022-12-24 21:44:56 +0800

    Self-host Inter fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
+	import "@mjrdd/inter";
 	import "../app.css";
 </script>
-
-<svelte:head>
-	<link rel="preconnect" href="https://rsms.me/" />
-	<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-</svelte:head>
 
 <slot />
