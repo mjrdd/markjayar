18899ba60178b1024bb840bec5a21dca673b7e64
Mark Devilleres <enigmaticjayar@gmail.com>
2022-12-25 15:07:04 +0800

    Fix text alignment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,16 +19,16 @@
 		display: grid;
 		place-items: center;
 		height: 100vh;
-		height: 100dvh;
 	}
 
 	h1 {
 		transition: all 0.3s ease-in-out;
 		margin-bottom: 1.5rem;
 		font-weight: 200;
-		font-size: 1.5rem;
+		font-size: 2rem;
 		letter-spacing: 1em;
 		text-align: center;
+		text-indent: 1em;
 
 		@media (min-width: 768px) {
 			font-size: 3rem;
