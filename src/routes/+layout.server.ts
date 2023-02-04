68aa47ce75b0144d476ccd39b3e5a80c4c0d429d
Mark Devilleres <mddevilleres@gmail.com>
2023-02-04 14:21:44 +0800

    Update +layout.server.ts

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,15 @@
 import type { LayoutServerLoad } from "./$types";
 
-export const load = (({ locals, getClientAddress }) => {
-	locals.pb.collection("visitors").create({
-		ip_address: getClientAddress()
-	});
+export const load = (async ({ locals, getClientAddress }) => {
+	try {
+		const record = await locals.pb.collection("visitors").create({
+			ip_address: getClientAddress()
+		});
+	} catch (err) {
+		console.log(err);
+	}
+
+	return {
+		ip: getClientAddress()
+	};
 }) satisfies LayoutServerLoad;
